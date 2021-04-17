0368bd90281f3beba07aa39ed9f65e00caff6274
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2021-04-17 03:23:39 -0700

    cargo fmt and add a count to the progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 use image::ImageBuffer;
+use indicatif::ParallelProgressIterator;
 use num::complex::Complex;
 use palette::{Gradient, Pixel, Srgb};
-use indicatif::ParallelProgressIterator;
+use rayon::iter::ParallelIterator;
 use rayon::prelude::*;
-use rayon::iter::{ParallelIterator};
-
 
 fn lerp(n: f64, domain: (f64, f64), range: (f64, f64)) -> f64 {
     let u = (n - domain.0) / (domain.1 - domain.0);
-    return u * (range.1 - range.0) + range.0;
+    u * (range.1 - range.0) + range.0
 }
 
 fn main() {
-    let width = 3840;
-    let height = 2160;
+    let width = 1920;
+    let height = 1080;
     let x_domain = (0.0, width as f64);
     let x_range = (-1.6, 1.6);
     let y_domain = (0.0, height as f64);
     let y_range = (-0.9, 0.9);
+    let max_iter = 1000;
     let c = Complex::new(-0.8, 0.156);
-    let fps = 24;
-    let seconds = 30;
+    let fps = 60;
+    let seconds = 23;
     let frames = fps * seconds;
     let zoom_factor: f64 = 0.99;
     let grad = Gradient::new(vec![
@@ -33,25 +33,28 @@ fn main() {
         Srgb::new(229.0 / 255.0, 181.0 / 255.0, 103.0 / 255.0).into_linear(),
         Srgb::new(214.0 / 255.0, 214.0 / 255.0, 214.0 / 255.0).into_linear(),
     ]);
-    (0..frames).into_par_iter().progress().for_each(|frame| {
-        let img = ImageBuffer::from_fn(width, height, |x, y| {
-            let zoom_factor= zoom_factor.powf(frame as f64);
-            let zoomed_x = (x_range.0 * zoom_factor, x_range.1 * zoom_factor);
-            let zoomed_y = (y_range.0 * zoom_factor, y_range.1 * zoom_factor);
-            let mut z = Complex::new(
-                lerp(x as f64, x_domain, zoomed_x),
-                lerp(y as f64, y_domain, zoomed_y),
-            );
-            let mut count = 0u8;
-            while z.norm() < 2.0 && count < 255 {
-                z = z * z + c;
-                count = count + 1;
-            }
-            let color: [u8; 3] = Srgb::from_linear(grad.get(count as f64 / 255.0))
-                .into_format()
-                .into_raw();
-            return image::Rgb(color);
+    (0..frames)
+        .into_par_iter()
+        .progress_count(frames)
+        .for_each(|frame| {
+            let img = ImageBuffer::from_fn(width, height, |x, y| {
+                let zoom_factor = zoom_factor.powf(frame as f64);
+                let zoomed_x = (x_range.0 * zoom_factor, x_range.1 * zoom_factor);
+                let zoomed_y = (y_range.0 * zoom_factor, y_range.1 * zoom_factor);
+                let mut z = Complex::new(
+                    lerp(x as f64, x_domain, zoomed_x),
+                    lerp(y as f64, y_domain, zoomed_y),
+                );
+                let mut count = 0;
+                while z.norm() < 2.0 && count < max_iter {
+                    z = z * z + c;
+                    count += 1;
+                }
+                let color: [u8; 3] = Srgb::from_linear(grad.get(count as f64 / max_iter as f64))
+                    .into_format()
+                    .into_raw();
+                image::Rgb(color)
+            });
+            img.save(format!("frames/{}.png", frame)).unwrap();
         });
-        img.save(format!("frames/{}.png", frame)).unwrap();
-    });
 }
